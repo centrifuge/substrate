532db929776e6e0c59ea1bd0452294870bac558f
vedhavyas <vedhavyas.singareddi@gmail.com>
2020-09-08 19:51:54 +0200

    update elections migrations

--- frame/elections-phragmen/src/migrations.rs
+++ frame/elections-phragmen/src/migrations.rs
@@ -1,17 +1,30 @@
 use super::*;
-use frame_support::{migration::{StorageKeyIterator, take_storage_item}, Twox64Concat};
+use frame_support::traits::MigrateAccount;
 
-pub fn migrate<T: Trait>() {
-	sp_runtime::print("🕊️  Migrating PhragmenElection...");
-	for (who, votes) in StorageKeyIterator
-		::<T::AccountId, Vec<T::AccountId>, Twox64Concat>
-	::new(b"PhragmenElection", b"VotesOf")
-		.drain()
-	{
-		if let Some(stake) = take_storage_item::<_, BalanceOf<T>, Twox64Concat>(b"PhragmenElection", b"StakeOf", &who) {
-			Voting::<T>::insert(who, (stake, votes));
-			sp_runtime::print("Phragmen: inserted Voting.");
+mod deprecated {
+	use super::*;
+	decl_module! {
+		pub struct Module<T: Trait> for enum Call where origin: T::Origin { }
+	}
+	decl_storage! {
+		trait Store for Module<T: Trait> as PhragmenElection {
+			/// Votes of a particular voter, with the round index of the votes.
+			pub VotesOf get(fn votes_of): map hasher(opaque_blake2_256) T::AccountId => Vec<T::AccountId>;
+			/// Locked stake of a voter.
+			pub StakeOf get(fn stake_of): map hasher(opaque_blake2_256) T::AccountId => BalanceOf<T>;
 		}
 	}
-	sp_runtime::print("🕊️  Done PhragmenElection.");
+}
+
+impl<T: Trait> MigrateAccount<T::AccountId> for Module<T> {
+	fn migrate_account(a: &T::AccountId) {
+		// Note: only migrates the hasher, migration is completed in `migration.rs`
+		if deprecated::StakeOf::<T>::contains_key(a) {
+			let locked_balance: BalanceOf<T> = deprecated::StakeOf::<T>::get(a);
+			let votes: Vec<T::AccountId> = deprecated::VotesOf::<T>::get(a);
+			Voting::<T>::insert(a, (locked_balance, votes));
+			deprecated::StakeOf::<T>::remove(a);
+			deprecated::VotesOf::<T>::remove(a);
+		}
+	}
 }
